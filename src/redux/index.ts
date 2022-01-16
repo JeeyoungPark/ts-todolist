d4536d41faa34ff788e54219c96b056c3e6a5938
jeeyoung_park <jeeyoung0321@gmail.com>
2022-01-16 22:29:39 +0900

    add redux-logger devtool

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,8 +1,13 @@
-import { combineReducers, createStore } from "redux"
+import { applyMiddleware, combineReducers, createStore } from "redux"
 import { tasks } from './tasks'
+import logger from 'redux-logger'
+import { composeWithDevTools } from "redux-devtools-extension"
 
 const rootReducer = combineReducers({ tasks })
 
-export const store = createStore(rootReducer);
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger)),
+);
 
 export type RootState = ReturnType<typeof rootReducer>;
