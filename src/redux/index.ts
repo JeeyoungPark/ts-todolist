038d934e34ed4b2956d76b435c6af6a65d5aaa0f
jeeyoung_park <jeeyoung0321@gmail.com>
2022-01-16 22:53:11 +0900

    add redux-persist

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -2,12 +2,27 @@ import { applyMiddleware, combineReducers, createStore } from "redux"
 import { tasks } from './tasks'
 import logger from 'redux-logger'
 import { composeWithDevTools } from "redux-devtools-extension"
+import { persistReducer } from "redux-persist"
+import persistStore from "redux-persist/lib/persistStore"
 
-const rootReducer = combineReducers({ tasks })
+import session from "redux-persist/lib/storage/session"
+// import storage from "redux-persist/lib/storage"
+
+const persistConfig = {
+  key: 'root',
+  storage: session,
+  whiteList: ['tasks'],
+}
+
+const combinedReducer = combineReducers({ tasks })
+
+const rootReducer = persistReducer(persistConfig, combinedReducer);
 
 export const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(logger)),
 );
+
+export const persistor = persistStore(store as any);
 
 export type RootState = ReturnType<typeof rootReducer>;
